7423d7225dd6a1abee1eacb921d973e90871016c
Nilesh Kumar <nileshkumar@geekyants.com>
2023-10-25 09:21:34 +0530

    feat/FE: Updated FE. Added display for response

--- public/index.js
+++ public/index.js
@@ -9,13 +9,18 @@ let OLD_ACCESS_TOKEN = "";
 let NEW_ACCESS_TOKEN = "";
 
 const optionSubmit = document.getElementById("option-submit");
-
-optionSubmit.addEventListener("click", (e) => {
+const loadingBtn = document.getElementById("loading-btn");
+const migrateResponseBlock = document.getElementById("migrate-response-block");
+const migrateResponseData = document.getElementById("migrate-response-data");
+optionSubmit.addEventListener("click", async (e) => {
   e.preventDefault();
-
+  optionSubmit.style.display = "none";
+  loadingBtn.style.display = "block";
   // darken the input field and disable it
   const oldAccAccessToken = document.getElementById("oldAccessToken");
   const newAccAccessToken = document.getElementById("newAccessToken");
+
+  migrateResponseBlock.style.display = "none";
 
   oldAccAccessToken.style.backgroundColor = "#e6e6e6";
   oldAccAccessToken.disabled = true;
@@ -45,20 +50,67 @@ optionSubmit.addEventListener("click", (e) => {
   }
 
   // hide the options
-  const subredditOptions = document.getElementById("subreddit-options");
-  subredditOptions.style.display = "none";
-  const savedPostsOptions = document.getElementById("saved-posts-options");
-  savedPostsOptions.style.display = "none";
-
-  console.log(OLD_ACCESS_TOKEN, NEW_ACCESS_TOKEN);
-  console.log(BOOL_MIGRATE_SUBREDDITS, BOOL_DELETE_SUBREDDITS);
-  console.log(BOOL_MIGRATE_SAVED_POSTS, BOOL_DELETE_SAVED_POSTS);
-
-  // show the progress bar
+  // const subredditOptions = document.getElementById("subreddit-options");
+  // subredditOptions.style.display = "none";
+  // const savedPostsOptions = document.getElementById("saved-posts-options");
+  // savedPostsOptions.style.display = "none";
 
   // start the migration
   console.log("Starting migration...");
+
+  const requestBody = {
+    old_account_cookie: OLD_ACCESS_TOKEN,
+    new_account_cookie: NEW_ACCESS_TOKEN,
+    preferences: {
+      migrate_subreddit_bool: BOOL_MIGRATE_SUBREDDITS,
+      migrate_post_bool: BOOL_MIGRATE_SAVED_POSTS,
+      delete_post_bool: BOOL_DELETE_SAVED_POSTS,
+      delete_subreddit_bool: BOOL_DELETE_SUBREDDITS,
+    },
+  };
+  const migrateResponse = await fetch("/api/migrate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
+  console.log(migrateResponse);
+  const response = await migrateResponse.json();
+  console.log(response);
+
+  if (migrateResponse.status == 200) {
+    displayMigrationResponse(response);
+  }
 });
+
+function displayMigrationResponse(response) {
+  const SubscribeSubreddit = document
+    .getElementById("subscribe-subreddit")
+    .querySelector("span");
+  SubscribeSubreddit.innerText = `Total subreddits successfully subscribed to new account: ${response.data.subscribeSubreddit.SuccessCount}`;
+
+  const UnsubscribeSubreddit = document
+    .getElementById("unsubscribe-subreddit")
+    .querySelector("span");
+  UnsubscribeSubreddit.innerHTML = `Total subreddits successfully unsubscribed from old account: ${response.data.unsubscribeSubreddit.SuccessCount}`;
+
+  const SavePost = document.getElementById("save-post").querySelector("span");
+  SavePost.innerHTML = `Total posts successfully saved in new account: ${response.data.savePost.SuccessCount}`;
+
+  const UnsavePost = document
+    .getElementById("unsave-post")
+    .querySelector("span");
+  UnsavePost.innerHTML = `Total posts successfully unsaved from old account: ${response.data.unsavePost.SuccessCount}`;
+
+  optionSubmit.style.display = "block";
+  loadingBtn.style.display = "none";
+  migrateResponseBlock.style.display = "block";
+  migrateResponseData.style.display = "block";
+  migrateResponseBlock.scrollIntoView({
+    behavior: "smooth",
+  });
+}
 
 const oldTokenVerifyBtn = document.getElementById("oldTokenVerifyBtn");
 const newTokenVerifyBtn = document.getElementById("newTokenVerifyBtn");
@@ -67,7 +119,8 @@ oldTokenVerifyBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const oldAccAccessToken = document.getElementById("oldAccessToken");
   const oldAccAccessTokenValue = oldAccAccessToken.value;
-  const verifyOldToken = await verifyToken(oldAccAccessTokenValue);
+  const verifyOldToken = await verifyCookie(oldAccAccessTokenValue);
+  console.log(verifyOldToken);
   if (verifyOldToken.success) {
     BOOL_OLD_TOKEN_VERIFIED = true;
     oldAccAccessToken.disabled = true;
@@ -79,6 +132,7 @@ oldTokenVerifyBtn.addEventListener("click", async (e) => {
     oldTokenVerifyBtn.innerHTML = "Verified";
     document.getElementById("oldTokenVerifySuccessMessage").style.display =
       "block";
+    document.getElementById("oldTokenVerifyFailMessage").style.display = "none";
     document.getElementById("oldAccountUserId").innerHTML =
       verifyOldToken.data.username;
     // verifyOldToken.data.username;
@@ -95,7 +149,7 @@ newTokenVerifyBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const newAccAccessToken = document.getElementById("newAccessToken");
   const newAccAccessTokenValue = newAccAccessToken.value;
-  const verifynewToken = await verifyToken(newAccAccessTokenValue);
+  const verifynewToken = await verifyCookie(newAccAccessTokenValue);
   if (verifynewToken.success) {
     BOOL_NEW_TOKEN_VERIFIED = true;
     newAccAccessToken.disabled = true;
@@ -107,6 +161,8 @@ newTokenVerifyBtn.addEventListener("click", async (e) => {
     newTokenVerifyBtn.innerHTML = "Verified";
     document.getElementById("newTokenVerifySuccessMessage").style.display =
       "block";
+    document.getElementById("newTokenVerifyFailMessage").style.display = "none";
+
     document.getElementById("newAccountUserId").innerHTML =
       verifynewToken.data.username;
     // verifynewToken.data.username;
@@ -150,9 +206,18 @@ document.getElementById("copy-button").addEventListener("click", (e) => {
     document.getElementById("copy-button").innerHTML = "Copy";
   }, 2000);
 });
-async function verifyToken(token) {
-  const response = await fetch("/api/verify-token", {
-    body: JSON.stringify({ access_token: token }),
+async function verifyCookie(cookie) {
+  const cookieData = getCookieObject(cookie);
+  if (cookieData.token_v2 === undefined) {
+    return {
+      success: false,
+      message: "Invalid Cookie. Please get a new one.",
+      data: {},
+    };
+  }
+
+  const response = await fetch("/api/verify-cookie", {
+    body: JSON.stringify({ cookie: cookie }),
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -164,7 +229,7 @@ async function verifyToken(token) {
   if (response.status !== 200) {
     return {
       success: false,
-      message: "Invalid access token",
+      message: "Invalid Cookie",
       data: {},
     };
   }
